e1b79021fee4bf9e396845da8bcbfe669a22e927
Aidan Beilke <aidanbeilke1@gmail.com>
2024-08-20 22:20:44 -0400

    Update data_gather.py

--- data_gather.py
+++ data_gather.py
@@ -39,21 +39,3 @@ def get_batting_stats_with_year(start_year, end_year, min_abs):
     all_data = all_data.merge(player_map, left_on='IDfg', right_on='key_fangraphs', how='left')
 
     return all_data
-
-def get_launch_angle_swsp(start_date, end_date, min_bbe):
-    data = statcast(start_date, end_date)
-    
-    data = data.dropna(subset=['launch_speed', 'events', 'launch_angle']) 
-    data['is_swsp'] = np.where((data['launch_angle'] >= 8) & (data['launch_angle'] <= 32), 1, 0)
-    swsp = data.groupby(['batter', 'game_year']).agg({
-        'is_swsp': 'mean',
-        'launch_angle': 'size'
-    }).reset_index().rename(columns={'launch_angle': 'bbe', 'batter': 'key_mlbam', 'game_year': 'Season'})
-
-    swsp = swsp[swsp['bbe'] >= min_bbe]
-
-    return swsp
-
-def join_all_df(bats_df, swsp_df):
-    merged_data = bats_df.merge(swsp_df, on = ['key_mlbam', 'Season'], how = 'inner')
-    return merged_data
