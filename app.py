64ab7ba3fcd0affff763542cb3898052337eeb42
Aidan Beilke <aidanbeilke1@gmail.com>
2024-08-20 23:05:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def plot_grouped_bar(data, selected_player, selected_stat):
     ax.set_title("Model Performance Through The Years", fontsize=50, loc='center')
     ax.set_xlabel('Season', fontweight='bold', fontsize = 20)
     ax.set_ylabel('Value', fontweight='bold', fontsize = 20)
-    ax.set_xticks([r + bar_width / 2 for r in range(len(seasons))], fontsize = 20)
+    ax.set_xticks([r + bar_width / 2 for r in range(len(seasons))])
     ax.set_xticklabels(seasons, fontsize = 20)
 
     # Add legend
